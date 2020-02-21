f2bf47b2891b3db0692eb3bb428c13dd241c67d4
idongliming <idongliming@qq.com>
2020-02-21 20:03:32 +0800

    完善后端，提供真接口
    - 删除写死的数据文件
    - 无关文件

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
 const app = express();
+
+const superagent = require('superagent');
+const cheerio = require('cheerio');
+const url = "https://ncov.dxy.cn/ncovh5/view/pneumonia";
 
 // 公共资源托管，public下的资源了以直接访问
 app.use('/',express.static('public'));
 
-app.get('/api/data', (req, res) => {
-    res.send("data")
+// 程序运行后爬取疫情信息
+var dataObj = {}
+superagent.get(url).then(res => {
+    const $ = cheerio.load(res.text)
+    var $getListByCountryTypeService1 = $('#getListByCountryTypeService1').html();
+    eval($getListByCountryTypeService1.replace(/window/g, 'dataObj'));
 })
+
+// api
+app.get('/api/data', (req, res) => {
+    res.send(JSON.stringify(dataObj))
+})
+
 app.listen(8088, () => {
     console.log("当前服务器启动成 http://127.0.0.1:8088/");
 })
